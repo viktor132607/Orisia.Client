e9aac122af4129ae93fac0e1e101b2004b8b0a2d
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-02 17:21:33 +0300

    Add English admin gallery copy

--- src/app/admin/gallery/page.tsx
+++ src/app/admin/gallery/page.tsx
@@ -1,5 +1,5 @@
 import AdminSectionPlaceholder from "../../../components/AdminSectionPlaceholder";
 
 export default function AdminGalleryPage() {
-  return <AdminSectionPlaceholder title="Галерия" description="Качване, подреждане и управление на снимките в галерията." />;
+  return <AdminSectionPlaceholder title="Галерия" description="Качване, подреждане и управление на снимките в галерията." titleEn="Gallery" descriptionEn="Upload, arrange and manage gallery photos." />;
 }
