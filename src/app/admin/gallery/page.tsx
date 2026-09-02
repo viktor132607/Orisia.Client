0993daeeded4cc6ee28043af1505ce7052f406bc
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-02 17:00:35 +0300

    Add admin gallery section

---
+++ src/app/admin/gallery/page.tsx
@@ -0,0 +1,5 @@
+import AdminSectionPlaceholder from "../../../components/AdminSectionPlaceholder";
+
+export default function AdminGalleryPage() {
+  return <AdminSectionPlaceholder title="Галерия" description="Качване, подреждане и управление на снимките в галерията." />;
+}
