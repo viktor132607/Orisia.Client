5f098d29a1339fbbbcc7536ca6bfbe99269e6d42
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-02 17:00:53 +0300

    Add admin users section

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,5 @@
+import AdminSectionPlaceholder from "../../../components/AdminSectionPlaceholder";
+
+export default function AdminUsersPage() {
+  return <AdminSectionPlaceholder title="Потребители" description="Управление на профили, роли и достъп до сайта." />;
+}
