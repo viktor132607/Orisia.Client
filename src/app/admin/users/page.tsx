d8482411893283dcd2c6b3215b894875b84f3d1e
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-02 17:21:58 +0300

    Add English admin users copy

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,5 +1,5 @@
 import AdminSectionPlaceholder from "../../../components/AdminSectionPlaceholder";
 
 export default function AdminUsersPage() {
-  return <AdminSectionPlaceholder title="Потребители" description="Управление на профили, роли и достъп до сайта." />;
+  return <AdminSectionPlaceholder title="Потребители" description="Управление на профили, роли и достъп до сайта." titleEn="Users" descriptionEn="Manage profiles, roles and access to the site." />;
 }
