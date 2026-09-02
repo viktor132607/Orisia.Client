064f50c0e008be099381013784d2c9fcae8aee41
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-02 17:01:13 +0300

    Add admin homepage section

---
+++ src/app/admin/home/page.tsx
@@ -0,0 +1,5 @@
+import AdminSectionPlaceholder from "../../../components/AdminSectionPlaceholder";
+
+export default function AdminHomePage() {
+  return <AdminSectionPlaceholder title="Начална страница" description="Управление на основните текстове, секции и акценти на началната страница." />;
+}
