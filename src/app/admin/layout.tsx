2ee80a4c17cbfb8b0e678aa54b66b87eb41931b6
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-25 00:24:42 +0300

    Prevent indexing of private routes

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,16 @@
+import type { Metadata } from "next";
 import AdminMenu from "../../components/AdminMenu";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
+};
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
